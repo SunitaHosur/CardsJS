3cbbee89868e32fd60113b64f1b57b920adfdab1
sunita hosur <sunitahosur81@gmail.com>
2021-06-08 10:46:54 +0530

    cards using js

--- main.js
+++ main.js
@@ -13,17 +13,17 @@ fetch('https://animechan.vercel.app/api/quotes/anime?title=naruto')
               card.appendChild(title)
               card.appendChild(content)
             
-            card.style.boxShadow = '0 4px 8px 0 rgba(0,0,0,0.2)';
-            card.style.width = '20rem';
-            card.style.marginTop = "2%";
-            card.style.borderRadius = "15px";
-            card.style.flexWrap = "wrap";
-              
-            title.style.color = "gold";
-            title.style.color = "gold";
+                card.style.boxShadow = '0 4px 8px 0 rgba(0,0,0,0.2)';
+                card.style.width = '20rem';
+                card.style.marginTop = "2%";
+                card.style.borderRadius = "15px";
+                card.style.flexWrap = "wrap";
+                
+                title.style.color = "gold";
+                title.style.color = "gold";
 
-            content.style.color = "peachpuff";
-            content.style.fontSize = "20px"
+                content.style.color = "peachpuff";
+                content.style.fontSize = "20px"
               
             return card;
 
